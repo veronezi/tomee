ceb9d00a500cf66060de2ffd4c426d8290273056
Thiago Veronezi <thiago@veronezi.org>
2018-11-07 14:59:15 -0500

    init

---
+++ .travis.deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+function deploy_docker_image () {
+    if [ "$TRAVIS_BRANCH" = "master" ]; then
+        echo "deploying docker image veronezi/$1:$2-b$3"
+        docker tag $1 veronezi/$1:$2-b$3
+        docker tag $1 veronezi/$1:latest
+        docker push veronezi/$1:$2-b$3
+        docker push veronezi/$1:latest
+    else
+        echo "deploying docker image veronezi/$1:$2-rc$3"
+       docker tag $1 veronezi/$1:$2-rc$3
+       docker push veronezi/$1:$2-rc$3
+    fi
+}
+
+deploy_docker_image sample-rendertron $MY_VERSION $TRAVIS_BUILD_NUMBER
+
+echo "binaries uploaded"
