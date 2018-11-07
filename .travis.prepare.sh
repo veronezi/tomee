779e01bfd9370f46c8ef2448812b2f0bc19d0241
Thiago Veronezi <thiago@veronezi.org>
2018-11-07 15:20:13 -0500

    fi version number

--- .travis.prepare.sh
+++ .travis.prepare.sh
@@ -5,7 +5,7 @@ cp .travis.settings.xml $HOME/.m2/settings.xml
 sed -i 's/my_password/'"$BINTRAY_PASSWORD"'/g' $HOME/.m2/settings.xml
 
 mvn clean -q
-mvn -q versions:set -DnewVersion=$TODO_VERSION-b$TRAVIS_BUILD_NUMBER
+mvn -q versions:set -DnewVersion=$MY_VERSION-b$TRAVIS_BUILD_NUMBER
 
 docker logout
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
