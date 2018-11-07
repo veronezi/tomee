36c3786453f2991b0e6a614db7f621f28e2d6a5c
Thiago Veronezi <thiago@veronezi.org>
2018-11-07 15:16:23 -0500

    fix deploy

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -15,6 +15,6 @@ function deploy_docker_image () {
     fi
 }
 
-deploy_docker_image sample-rendertron $MY_VERSION $TRAVIS_BUILD_NUMBER
+deploy_docker_image sample-tomee $MY_VERSION $TRAVIS_BUILD_NUMBER
 
 echo "binaries uploaded"
